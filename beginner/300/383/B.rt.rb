fdfa355c7be34d1ae205748004893f981de055f6
ikeyaH <ta0921ni@gmail.com>
2024-12-08 08:40:16 +0900

    20241208 - 2

---
+++ beginner/300/383/B.rt.rb
@@ -0,0 +1,41 @@
+# すべての点に対して、1度確認すればOKなコードにする
+H, W, D = gets.chomp.split(' ').map(&:to_i)
+grid = []
+H.times do
+  grid << gets.chomp.split('')
+end
+
+result = 0
+
+for i in 0...H do
+  for j in 0...W do
+    target_1 = grid[i][j]
+    next if target_1 != '.'
+
+
+
+    count = 0
+    for k in 0...H do
+      for l in 0...W do
+        target_2 = grid[k][l]
+        next if k == i && l == j
+        next if target_2 != '.'
+
+        count = 0
+        for m in 0...H do
+          for n in 0...W do
+            # どちらかからの距離がD以下、'.'である時
+            if grid[m][n] != '#'
+              if  (((i - m).abs + (j - n).abs) <= D) || (((k - m).abs + (l - n).abs <= D))
+                count += 1
+              end
+            end
+          end
+        end
+        result = count if result < count
+      end
+    end
+  end
+end
+
+puts result
