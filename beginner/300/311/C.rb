ff9060c1582181e33f5a8c8e4e98fdbcbd6dac6b
ikeyaH <ta0921ni@gmail.com>
2025-01-30 20:29:48 +0900

    20250130 - 1

--- beginner/300/311/C.rb
+++ beginner/300/311/C.rb
@@ -1,1 +1,25 @@
-# グラフ
+# https://atcoder.jp/contests/abc311/tasks/abc311_c
+
+N = gets.to_i
+targets = gets.chomp.split(' ').map(&:to_i)
+
+# どこからそこに来たか、を記すもの
+from = Array.new(N + 1, 0)
+reco = []
+now = 0
+result = nil
+(N + 1).times do |i|
+  target = targets[now]
+  reco.push(target)
+  if from[target] == 0
+    from[target] = now + 1
+    now = target - 1
+  else
+    f = reco.find_index(target)
+    result = reco.slice(f...i)
+    break
+  end
+end
+
+puts result.length
+puts result.join(' ')
