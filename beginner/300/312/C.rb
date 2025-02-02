4af8c7a06ea0f3f8fef780731cf0074c5e92a199
ikeyaH <ta0921ni@gmail.com>
2025-02-02 09:55:31 +0900

    20250201 - 1

--- beginner/300/312/C.rb
+++ beginner/300/312/C.rb
@@ -1,6 +1,22 @@
 N,M = gets.chomp.split(' ').map(&:to_i)
-sell_price = gets.chomp.split(' ').map(&:to_i).sort
-by_price = gets.chomp.split(' ').map(&:to_i).sort
+sell_price = gets.chomp.split(' ').map(&:to_i).tally
+# 買い手は、その値段なら買う = その値段 +1したら買う人の人数から減らして良いので、+1する。
+by_price = gets.chomp.split(' ').map{ |n| n.to_i+1 }.tally
 
-p sell_price
-pp by_price
+sell_price.default = 0
+by_price.default = 0
+
+keys = (sell_price.keys | by_price.keys).sort
+
+s_num = 0
+b_num = M
+
+keys.each do |k|
+  s_num += sell_price[k]
+  b_num -= by_price[k]
+
+  if s_num >= b_num
+    puts k
+    exit
+  end
+end
