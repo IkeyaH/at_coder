c4d83d16a7d1421c9e4b1c3f3f11b77cc0e88f02
ikeyaH <ta0921ni@gmail.com>
2025-03-27 21:11:06 +0900

    20250327 - 1

--- beginner/300/356/C.rt6.rb
+++ beginner/300/356/C.rt6.rb
@@ -1,1 +1,34 @@
-# todo
+N, M, K = gets.split.map(&:to_i)
+kb = Array.new(M, 0)
+r = Array.new(M)
+
+def popcount(num)
+  count = 0
+  while num > 0
+    num &= (num - 1)
+    count += 1
+  end
+  count
+end
+
+
+M.times do |i|
+  ta = gets.chomp.split
+  r[i] = ta[-1]
+
+  a = ta[1..(ta[0]).to_i]
+  a.each do |b|
+    kb[i] |= (1<<(b.to_i - 1))
+  end
+end
+
+ans = 0
+(1<<N).times do |bit|
+  ok = true
+  M.times do |i|
+    ok = false if (popcount(bit & kb[i]) >= K) != (r[i] == 'o')
+  end
+  ans += 1 if ok
+end
+
+puts ans
--- beginner/300/358/C.rt5.rb
+++ beginner/300/358/C.rt5.rb
@@ -1,1 +1,29 @@
-# todo
+N, M = gets.split.map(&:to_i)
+tasts = Array.new(N, 0)
+
+def popcount(num)
+  count = 0
+  while num > 0
+    num &= (num - 1)
+    count += 1
+  end
+  count
+end
+
+N.times do |i|
+  ta = gets.chomp.chars.reverse
+  ta.each_with_index do |t, j|
+    tasts[i] |= (1<<j) if t == 'o'
+  end
+end
+
+ans = N
+(1<<N).times do |bit|
+  can_t = 0
+  N.times do |i|
+    can_t |= tasts[i] if (bit>>i&1 == 1)
+  end
+  ans = [ans, popcount(bit)].min if popcount(can_t) == M
+end
+
+puts ans
