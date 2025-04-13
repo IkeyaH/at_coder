6d8216875e95a7d9d7640480fe3da03c70f37ffb
ikeyaH <ta0921ni@gmail.com>
2025-04-13 15:26:35 +0900

    別環境から

--- beginner/300/372/C.rt2.rb
+++ beginner/300/372/C.rt2.rb
@@ -1,1 +1,26 @@
-# todo
+N, Q = gets.split.map(&:to_i)
+s = gets.chomp
+
+count = 0
+(N-2).times do |i|
+  count += 1 if s[i,3] == 'ABC'
+end
+
+ans = []
+Q.times do
+  _x, c = gets.chomp.split
+  x = _x.to_i - 1
+  # 関わる場所にABCがあれば引いておく
+  3.times do |i|
+    count -= 1 if s[x-i, 3] == 'ABC'
+  end
+  # 入れ替え
+  s[x] = c
+  # 入れ替え後のABC数
+  3.times do |i|
+    count += 1 if s[x-i, 3] == 'ABC'
+  end
+  ans << count
+end
+
+puts ans
