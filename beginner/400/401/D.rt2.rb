d350e4e1a58184af272cd7beac31be99de7c8b8f
ikeyaH <ta0921ni@gmail.com>
2025-04-16 08:04:31 +0900

    20240416 - 1

--- beginner/400/401/D.rt2.rb
+++ beginner/400/401/D.rt2.rb
@@ -1,16 +1,14 @@
-# todo: 途中だで！
 N, K = gets.split.map(&:to_i)
 s = gets.chomp
 
 N.times do |i|
-  if N[i] == 'o'
-    s[i-1] = '.' if i-1 > 0
+  if s[i] == 'o'
+    s[i-1] = '.' if i-1 >= 0
     s[i+1] = '.' if i+1 < N
   end
 end
 
 oc = s.count('o')
-
 if oc == K
   N.times do |i|
     s[i] = '.' if s[i] == '?'
@@ -21,11 +19,11 @@ end
 
 s_dup = s.dup
 N.times do |i|
-  if s[i] == '?' 
+  if s_dup[i] == '?' 
     if i == 0
-      s[i] = 'o'
+      s_dup[i] = 'o'
     else
-      s[i] = 'o' if s[i-1] != 'o'
+      s_dup[i] = 'o' if s_dup[i-1] != 'o'
     end
   end
 end
@@ -35,6 +33,28 @@ ocm = s_dup.count('o')
 if ocm > K
   puts s
   exit
-else
-
 end
+
+oa = []
+boc = 0
+N.times do |i|
+  if s[i] == '?'
+    boc += 1
+    oa << [i-boc+1, boc] if i == (N-1)
+  else
+    oa << [i-boc, boc] if boc > 0
+    boc = 0
+  end
+end
+
+oa.each do |x, y|
+  next if y.even?
+  y.times do |i|
+    if i.even?
+      s[x+i] = 'o'
+    else
+      s[x+i] = '.'
+    end
+  end
+end
+puts s
