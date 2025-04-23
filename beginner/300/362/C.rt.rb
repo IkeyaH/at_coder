9cccdfa2d9f50ed72ae10163cb3d0c9ed0c1848a
ikeyaH <ta0921ni@gmail.com>
2025-04-23 22:16:53 +0900

    20250423 - 1

--- beginner/300/362/C.rt.rb
+++ beginner/300/362/C.rt.rb
@@ -1,1 +1,30 @@
-# TODO
+N = gets.to_i
+la = []
+ra = []
+
+N.times do
+  l, r = gets.split.map(&:to_i)
+  la << l
+  ra << r
+end
+
+min = la.sum
+max = ra.sum
+if min > 0 || max < 0
+  puts 'No'
+  exit
+end
+
+ans = la.dup
+N.times do |i|
+  if min + (ra[i] - la[i]) <= 0
+    ans[i] = ra[i]
+    min = min + (ra[i] - la[i])
+  else
+    ans[i] = la[i] + min.abs
+    break
+  end
+end
+
+puts 'Yes'
+puts ans.join(' ')
