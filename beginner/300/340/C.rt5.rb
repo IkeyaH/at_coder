7764dfe1795d81bd5a452a1b196efccb40ba335a
ikeyaH <ta0921ni@gmail.com>
2025-04-24 22:06:51 +0900

    20250424 - 2

--- beginner/300/340/C.rt5.rb
+++ beginner/300/340/C.rt5.rb
@@ -1,1 +1,12 @@
 # todo
+
+N = gets.to_i
+
+def calc(h, n)
+  return 0 if n < 2
+  return h[n] if h.key?(n)
+  h[n] = n + calc(h, (n+1)/2) + calc(h, n/2)
+end
+
+ha = {}
+puts calc(ha, N)
--- beginner/300/358/C.rt6.rb
+++ beginner/300/358/C.rt6.rb
@@ -1,1 +1,36 @@
 # TODO
+
+def popcount_kernighan(num)
+  count = 0
+  while num > 0
+    num &= (num - 1)
+    count += 1
+  end
+  count
+end
+
+N, M = gets.split.map(&:to_i)
+shop = []
+N.times { shop << gets.chomp }
+shop_tasts = Array.new(N, 0)
+
+N.times do |i|
+  M.times do |j|
+    if shop[i][j] == 'o'
+      shop_tasts[i] |= (1<<j)
+    end
+  end
+end
+
+ans = N
+(1<<N).times do |bit|
+  tasts = 0
+  N.times do |m|
+    if bit>>m&1 == 1
+      tasts |= shop_tasts[m]
+    end
+  end
+  ans = [ans, popcount_kernighan(bit)].min if popcount_kernighan(tasts) == M
+end
+
+puts ans
