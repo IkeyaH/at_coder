2e65156e08d33977d97c8659af06d80f71bbbb99
ikeyaH <ta0921ni@gmail.com>
2025-04-29 16:46:41 +0900

    20250429 - 3

--- beginner/300/367/C.rt.rb
+++ beginner/300/367/C.rt.rb
@@ -1,1 +1,22 @@
-# TODO
+N, K = gets.split.map(&:to_i)
+R = gets.split.map(&:to_i)
+
+ANS = []
+
+def f(arr)
+  as = arr.size
+  if as == N
+    ANS << arr.join(' ') if arr.sum % K == 0
+    return
+  end
+
+  R[as].times do |i|
+    na = arr.dup
+    na << i+1
+    f(na)
+  end
+end
+
+f([])
+
+puts ANS
