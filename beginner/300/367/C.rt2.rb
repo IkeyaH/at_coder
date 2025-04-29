a25de55240b44a8eab6df2e3f45fd6b437575640
ikeyaH <ta0921ni@gmail.com>
2025-04-29 17:14:20 +0900

    20250429 - 4

--- beginner/300/367/C.rt2.rb
+++ beginner/300/367/C.rt2.rb
@@ -1,1 +1,22 @@
-# todo
+N, K = gets.split.map(&:to_i)
+R = gets.split.map(&:to_i)
+
+A = []
+
+def f(arr)
+  as = arr.size
+  if as == N
+    A << arr.join(' ') if arr.sum % K == 0
+    return
+  end
+
+  R[as].times do |i|
+    na = arr.dup
+    na << i + 1
+    f(na)
+  end
+end
+
+f([])
+
+puts A
--- beginner/300/383/C.rt3.rb
+++ beginner/300/383/C.rt3.rb
@@ -1,1 +1,39 @@
-# todo
+H, W, D = gets.split.map(&:to_i)
+grids = []
+H.times { grids << gets.chomp }
+num_grids = Array.new(H) { Array.new(W, Float::INFINITY) }
+
+q = Queue.new
+
+H.times do |i|
+  W.times do |j|
+    if grids[i][j] == 'H'
+      num_grids[i][j] = 0
+      q.push([i, j])
+    end
+  end
+end
+
+di = [1, 0, -1, 0]
+dj = [0, 1, 0, -1]
+
+while q.size > 0
+  base_i, base_j = q.pop
+  4.times do |i|
+    ni, nj = base_i+di[i], base_j+dj[i]
+    next if ni < 0 || ni >= H || nj < 0 || nj >= W
+    next if num_grids[ni][nj] != Float::INFINITY
+    next if grids[ni][nj] == '#'
+    num_grids[ni][nj] = num_grids[base_i][base_j] + 1
+    q.push([ni, nj])
+  end
+end
+
+ans = 0
+H.times do |i|
+  W.times do |j|
+    ans += 1 if num_grids[i][j] <= D
+  end
+end
+
+puts ans
