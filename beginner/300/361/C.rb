f59c5ad078d5116e73c9d45890695ba914ea4280
ikeyaH <ta0921ni@gmail.com>
2025-04-30 20:57:45 +0900

    20250430 - 1

--- beginner/300/361/C.rb
+++ beginner/300/361/C.rb
@@ -1,23 +1,3 @@
-# N, M = gets.split.map(&:to_i)
-# aa = gets.split.map(&:to_i).sort
-
-# M.times do
-#   pp aa
-#   f, s = aa[0], aa[1]
-#   l, sl = aa[-1], aa[-2]
-
-#   f_diff = s - f
-#   l_diff  = l - sl
-
-#   if f_diff >= l_diff
-#     aa.shift
-#   else
-#     aa.pop
-#   end
-# end
-
-# puts aa[-1] - aa[0]
-
 N, M = gets.split.map(&:to_i)
 aa = gets.split.map(&:to_i).sort
 
--- beginner/300/361/C.rt.rb
+++ beginner/300/361/C.rt.rb
@@ -1,1 +1,13 @@
-# TODO
+N, K = gets.split.map(&:to_i)
+A = gets.split.map(&:to_i).sort
+
+W = N-K-1
+
+ans = Float::INFINITY
+(W).upto(N-1) do |i|
+  min = A[i-W]
+  max = A[i]
+  ans = max - min if max - min < ans
+end
+
+puts ans
