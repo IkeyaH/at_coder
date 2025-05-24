7d5d472b3fd92a11f01a85f40e75e0c4d55217be
ikeyaH <ta0921ni@gmail.com>
2025-05-24 09:30:03 +0900

    20250524 - 1

--- beginner/400/406/C.rb
+++ beginner/400/406/C.rb
@@ -1,15 +1,29 @@
 N = gets.to_i
 arr = gets.split.map(&:to_i)
 
-la = []
-sm = []
+d = []
+(N-1).times { |i| (arr[i] < arr[i+1]) ? d << 0 : d << 1 }
 
-1.upto(N-2) do |i|
-  la << i if arr[i-1] < arr[i] && arr[i] > arr[i+1]
-  sm << i if arr[i-1] > arr[i] && arr[i] < arr[i+1]
+Pair = Struct.new(:number, :cnt)
+rle = []
+d.each do |x|
+  if rle.size > 0 && rle[-1].number == x
+    rle[-1].cnt += 1
+  else
+    rle << Pair.new(x, 1)
+  end
 end
 
+ans = 0
 
-N.times do |i|
-  
+(rle.length).times do |i|
+  if rle[i].number == 1
+    l, r = 0, 0
+    l = rle[i-1].cnt if i > 0
+    r = rle[i+1].cnt if i+1 < rle.length
+
+    ans += l * r
+  end
 end
+
+puts ans
