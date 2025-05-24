8a22649cf34be14ad505516a463272b69c27f763
ikeyaH <ta0921ni@gmail.com>
2025-05-24 16:31:58 +0900

    20250524 - 3

--- beginner/300/341/C.rt3.rb
+++ beginner/300/341/C.rt3.rb
@@ -1,1 +1,29 @@
-# todo
+H, W, N = gets.split.map(&:to_i)
+T = gets.chomp
+dire = {
+  'L' => -1,
+  'R' => 1,
+  'U' => -W,
+  'D' => W
+}
+
+
+grids = []
+H.times { grids << gets.chomp }
+gins = ''
+
+grids.each do |g|
+  gins += g.tr('#.', '01')
+end
+
+start = gins.to_i(2)
+ans = start
+
+d = 0
+T.each_char do |t|
+  d += dire[t]
+  now = start << d
+  ans = ans & now
+end
+
+puts ans.to_s(2).count('1')
