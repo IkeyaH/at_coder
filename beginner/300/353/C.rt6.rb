d3b47005a3d88f0c5c42a49423a9ab29de41e626
ikeyaH <ta0921ni@gmail.com>
2025-05-27 08:02:24 +0900

    20250527 - 1

--- beginner/300/353/C.rt6.rb
+++ beginner/300/353/C.rt6.rb
@@ -1,1 +1,15 @@
-# TODO
+B = 10**8
+N = gets.to_i
+num = gets.chomp.split.map(&:to_i).sort
+
+sum = num.sum * (N-1)
+ans = sum
+r = N-1
+
+(N-1).times do |i|
+  r -= 1 while i < r && (num[i] + num[r]) >= B
+  ma = [r, i].max
+  ans -= B*(N-1-ma)
+end
+
+puts ans
