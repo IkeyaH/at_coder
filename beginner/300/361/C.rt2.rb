3d6b2e36761d82f504091e0d69a741a2e8380cd8
ikeyaH <ta0921ni@gmail.com>
2025-05-29 08:49:08 +0900

    20250529 - 2

--- beginner/300/361/C.rt2.rb
+++ beginner/300/361/C.rt2.rb
@@ -1,1 +1,13 @@
-# todo
+N, K = gets.split.map(&:to_i)
+arr = gets.split.map(&:to_i).sort
+w = N - K - 1
+
+ans = Float::INFINITY
+
+N.times do |i|
+  break if i + w >= N
+  diff = arr[i+w] - arr[i]
+  ans = [diff, ans].min
+end
+
+puts ans
