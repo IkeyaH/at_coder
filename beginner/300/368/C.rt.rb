c3e547f5c8fcd7e405e121968d63c30bcba1dec2
ikeyaH <ta0921ni@gmail.com>
2025-05-30 08:27:43 +0900

    20250530 - 3

--- beginner/300/368/C.rt.rb
+++ beginner/300/368/C.rt.rb
@@ -1,1 +1,18 @@
-# TODO: もうちょい綺麗にしてみる
+N = gets.to_i
+H = gets.split.map(&:to_i)
+
+ans = 0
+
+N.times do |i|
+  rem = H[i] % 5
+  quo = H[i] / 5
+
+  ans += quo * 3
+  while rem > 0
+    ans += 1
+    n = ans % 3 == 0 ? 3 : 1
+    rem -= n
+  end
+end
+
+puts ans
