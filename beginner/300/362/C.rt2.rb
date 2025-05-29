5215e0bdb4e4fc683d9138f32feeedbc147bda11
ikeyaH <ta0921ni@gmail.com>
2025-05-29 09:04:24 +0900

    20250529 - 3

--- beginner/300/362/C.rt2.rb
+++ beginner/300/362/C.rt2.rb
@@ -1,1 +1,33 @@
-# todo
+# todo: wa!
+N = gets.to_i
+Pair = Struct.new(:left, :right)
+arr = []
+
+min = 0
+ans = []
+N.times do
+  l, r = gets.split.map(&:to_i)
+  arr << Pair.new(l, r)
+  ans << l
+  min += l
+end
+
+if min > 0
+  puts 'No'
+  exit
+end
+
+r = min
+N.times do |i|
+  diff = arr[i].right - arr[i].left
+  if diff + r >= 0
+    ans[i] = arr[i].left - r
+    break
+  else
+    r += diff
+    ans[i] = arr[i].right
+  end
+end
+
+puts 'Yes'
+puts ans.join(' ')
