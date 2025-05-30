e4114fed9c26ebf605e27390eb9985777ab73a3e
ikeyaH <ta0921ni@gmail.com>
2025-05-31 08:16:55 +0900

    20250531 - 1

--- beginner/300/367/C.rt4.rb
+++ beginner/300/367/C.rt4.rb
@@ -1,1 +1,23 @@
-# todo
+N, K = gets.split.map(&:to_i)
+R = gets.split.map(&:to_i)
+
+A = []
+def f(a)
+  len = a.length
+
+  if len == N
+    A << a.join(' ') if a.sum % K == 0
+    return
+  end
+  # na = a.dup
+  # ここでdupすると、下の繰り返しで共有される
+  # = 要素数がNを超え、nilアクセスとなる
+  R[len].times do |i|
+    na = a.dup.push(i+1)
+    f(na)
+  end
+end
+
+f([])
+
+puts ans
