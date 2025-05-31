debca9406661b5933a1689ab3c2afbbedbc2379d
ikeyaH <ta0921ni@gmail.com>
2025-05-31 09:25:35 +0900

    20250531 - 5

--- beginner/300/369/C.rt.rb
+++ beginner/300/369/C.rt.rb
@@ -1,1 +1,20 @@
-# TODO: もうちょい綺麗にしてみる
+N = gets.to_i
+A = gets.split.map(&:to_i)
+
+ans = 1
+d = Float::INFINITY
+l = 1
+
+1.upto(N-1).each do |i|
+  nd = A[i-1] - A[i]
+  if d == nd
+    l += 1
+    ans += l
+  else
+    l = 2
+    ans += l
+    d = nd
+  end
+end
+
+puts ans
