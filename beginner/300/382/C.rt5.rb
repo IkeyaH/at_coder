3e0903652f72ffc221f740aa27b2f681b26306c7
ikeyaH <ta0921ni@gmail.com>
2025-06-05 08:24:32 +0900

    20250605 - 1

--- beginner/300/382/C.rt5.rb
+++ beginner/300/382/C.rt5.rb
@@ -1,19 +1,18 @@
 N, M = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
 B = gets.split.map(&:to_i)
-bm = B.max
 
-ids = Array.new(bm+1, -1)
+bm = B.max
+eat_person_ids = Array.new(bm+1, -1)
 
 N.times do |i|
   ta = A[i]
   if ta <= bm
-    ids[ta..bm] = Array.new((bm-ta+1), i+1)
-    bm = ta-1
+    eat_person_ids[ta..bm] = Array.new((bm-ta+1), i+1)
+    bm = ta - 1
   end
 end
 
 B.each do |b|
-  puts ids[b]
+  puts eat_person_ids[b]
 end
-
