7585cc83ffe47708880f8bd79eecf1e7a640c632
ikeyaH <ta0921ni@gmail.com>
2025-06-05 08:24:19 +0900

    20250605 - 1

--- beginner/400/408/C.rt.rb
+++ beginner/400/408/C.rt.rb
@@ -1,1 +1,18 @@
-# TODO
+N, M = gets.split.map(&:to_i)
+covered = Array.new(N+1, 0)
+
+M.times do
+  l, r = gets.split.map(&:to_i)
+  l, r = l - 1, r - 1
+  covered[l] += 1
+  covered[r+1] -= 1
+end
+
+cum = Array.new(N, 0)
+n = 0
+N.times do |i|
+  n += covered[i]
+  cum[i] = n
+end
+
+puts cum.sort[0]
