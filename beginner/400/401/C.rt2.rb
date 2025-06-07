1dddaf28dbfcaf6ebc095f8b6f4ad1c4a0efa7e2
ikeyaH <ta0921ni@gmail.com>
2025-06-07 09:32:09 +0900

    20250607 - 2

--- beginner/400/401/C.rt2.rb
+++ beginner/400/401/C.rt2.rb
@@ -1,1 +1,14 @@
-#todo
+N, K = gets.split.map(&:to_i)
+
+result = Array.new(10**6, 1)
+(N+1).times do |i|
+  next if i < K
+  if i == K
+    result[i] = K
+    next
+  end
+
+  result[i] = (result[i-1] * 2 - result[i-K-1]) % 10**9
+end
+
+puts result[N]
