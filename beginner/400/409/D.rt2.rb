e654420d04431d537bae562026f5413c08c3413f
ikeyaH <ta0921ni@gmail.com>
2025-06-12 08:05:15 +0900

    20240612 - 2

--- beginner/400/409/D.rt2.rb
+++ beginner/400/409/D.rt2.rb
@@ -1,1 +1,38 @@
-# todo
+N = gets.to_i
+
+ans = []
+
+N.times do
+  n = gets.to_i
+  s = gets.chomp
+  len = s.length
+
+  ss = nil
+  si = nil
+  (len-1) .times do |i|
+    ts = s[i]
+    ns = s[i+1]
+    if ts > ns
+      ss = ts
+      si = i
+      break
+    end
+  end
+
+  if ss.nil?
+    ans << s
+    next
+  end
+
+  (si+1).upto(len).each do |j|
+    if j == len || ss < s[j]
+      tmp = s[(si+1)..(j-1)]
+      tmp += ss
+      s[si..(j-1)] = tmp
+      ans << s
+      break
+    end
+  end
+end
+
+puts ans
