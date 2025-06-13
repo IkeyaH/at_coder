fb0378e0f2b27d4c9a9b18b4ffbafeca023889e4
ikeyaH <ta0921ni@gmail.com>
2025-06-14 07:58:11 +0900

    20250614 - 1

--- beginner/400/404/C.rt2.rb
+++ beginner/400/404/C.rt2.rb
@@ -1,1 +1,82 @@
-# todo
+# Union-Find
+class UnionFind
+  attr_reader :par, :siz
+
+  # 初期化
+  def initialize(n)
+    # 各要素の親ノードを保持する
+    @par = Array.new(n, -1)
+    # グループのサイズを保持する
+    @siz = Array.new(n, 1)
+  end
+
+  # 根を求める
+  def root(x)
+    if par[x] == -1
+      return x
+    else
+      # 経路圧縮のため、rootを再起的に呼び出し、-1になるまで行う
+      return par[x] = root(par[x])
+    end
+  end
+
+  # x, y が同じグループに属するかどうか（根が一致するかどうか）
+  def same?(x, y)
+    return root(x) == root(y)
+  end
+
+  # x を含むグループと y を含むグループとを併合
+  def unite(x, y)
+    # それぞれの根を取得
+    x = root(x)
+    y = root(y)
+
+    # 同じグループであれば早期リターン
+    return false if x == y
+
+    # y 側のサイズが小さくなるようにする
+    x, y = y, x if siz[x] < siz[y]
+
+    # y を x　の子とする
+    par[y] = x
+    siz[x] += siz[y]
+    return true
+  end
+
+  # グループのサイズを返す
+  def size(x)
+    return siz[root(x)]
+  end
+end
+
+N, M = gets.split.map(&:to_i)
+
+points = Array.new(N) { Set.new }
+
+M.times do
+  _a, _b = gets.split.map(&:to_i)
+  a, b = _a-1, _b-1
+  points[a].add(b)
+  points[b].add(a)
+end
+
+N.times do |i|
+  if points[i].length != 2
+    puts 'No'
+    exit
+  end
+end
+
+uf = UnionFind.new(N)
+
+N.times do |i|
+  p1, p2 = points[i].to_a
+  uf.unite(i, p1)
+  uf.unite(i, p2)
+end
+
+if uf.size(0) == N
+  puts 'Yes'
+else
+  puts 'No'
+end
