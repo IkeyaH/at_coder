9decf5d6e30512cebe3e6b758989068c98f90cea
ikeyaH <ta0921ni@gmail.com>
2025-06-23 18:00:10 +0900

    20250623 - 1

--- beginner/300/341/C.rt5.rb
+++ beginner/300/341/C.rt5.rb
@@ -1,1 +1,30 @@
-# todo
+H, W, N = gets.split.map(&:to_i)
+dire = {
+  "L" => -1,
+  "R" => 1,
+  "U" => -W,
+  "D" => W,
+}
+
+
+T = gets.chomp
+grids = []
+H.times { grids << gets.chomp }
+gs = ''
+
+grids.each do |g|
+  t = g.tr('.#', '10')
+  gs += t
+end
+
+sgi = gs.to_i(2)
+now  = sgi.dup
+d = 0
+
+N.times do |i|
+  d += dire[T[i]]
+
+  now = now & (sgi << d)
+end
+
+puts now.to_s(2).count('1')
