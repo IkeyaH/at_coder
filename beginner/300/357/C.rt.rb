e67d72a6e582d62e446f8a997b95c50fedd772f3
ikeyaH <ta0921ni@gmail.com>
2025-06-23 18:29:29 +0900

    20250623 -2

--- beginner/300/357/C.rt.rb
+++ beginner/300/357/C.rt.rb
@@ -1,1 +1,30 @@
-# todo
+# 25分！うーん。もう一回。
+# 最初よりよくはなってきている
+
+c = Array.new(7) { Array.new }
+c[0] = ["#"]
+
+N = gets.to_i
+
+1.upto(6).each do |i|
+  l = 3**i
+  bl = 3**(i-1)
+
+  l.times do |n|
+    tr = ''
+    l.times do |m|
+      if (n >= bl && n < 2*bl) && (m >= bl && m < 2*bl)
+        tr += "."
+      else
+        bn = n % bl
+        bm = m % bl
+        tr += c[i-1][bn][bm]
+      end
+    end
+    c[i] << tr
+  end
+end
+
+c[N].each do |t|
+  puts t
+end
